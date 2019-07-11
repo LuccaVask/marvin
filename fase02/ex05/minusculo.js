3e1b74fbc89673d58425105c0a637dc253aafcb7
LuccaVask <52722875+LuccaVask@users.noreply.github.com>
2019-07-11 20:02:37 -0300

    Deus Abençoe Porraaaa

---
+++ fase02/ex05/minusculo.js
@@ -0,0 +1,5 @@
+function minusculo(ronaldinhosoccer) {
+    var final = ronaldinhosoccer.toUpperCase().substring(1)
+    var inicio = ronaldinhosoccer.substr(0,1).toLowerCase();
+    return inicio + final;
+}
