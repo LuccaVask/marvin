8766b1e8ef142bc9500733db2076ba7f2117f3d7
LuccaVask <52722875+LuccaVask@users.noreply.github.com>
2019-07-23 19:22:38 -0300

    sdfsd

---
+++ fase04/ex01/nove.js
@@ -0,0 +1,9 @@
+function nove(deus){
+    var balero = deus.toString()
+    var jesus = balero.substr(-1,1)
+    if (jesus === "9" ) {
+        return "true" ;
+    } else {
+        return "false";
+    }
+}
