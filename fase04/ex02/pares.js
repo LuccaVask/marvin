34723fae197f65c297375c128d91872b9dc23337
LuccaVask <52722875+LuccaVask@users.noreply.github.com>
2019-07-23 20:00:00 -0300

    asapas

--- fase04/ex02/pares.js
+++ fase04/ex02/pares.js
@@ -1,5 +1,5 @@
 function pares(sam,frodo){
-    if (sam,frodo % 2 === 0){
+    if (sam,frodo % 2 === "0"){
         return "Os números são pares" ;
     } else {
         return "Os números não são pares" ;
