cffb6b9ba4eea6d0aa691401c01729b1bd4bbb1f
LuccaVask <52722875+LuccaVask@users.noreply.github.com>
2019-07-23 19:59:17 -0300

    ffsdf

---
+++ fase04/ex03/vogalOuConsoante.js
@@ -0,0 +1,9 @@
+
+function vogalOuConsoante(napolitano){
+    var enrolado = napolitano.toLowerCase()
+    if (enrolado == ) {
+        return "Vogal" ;
+    } else {
+        return "consoante" ;
+    }
+}
