4f99acb9f14ceff54f3bcbb531da872630c958a5
LuccaVask <52722875+LuccaVask@users.noreply.github.com>
2019-07-24 19:43:11 -0300

    dfsff

--- fase04/ex04/abc.js
+++ fase04/ex04/abc.js
@@ -2,8 +2,8 @@ function abc(palavra){
     var nome = palavra.toLowerCase()
     var cel = nome.substr(0,1)
     if (cel == "a" || cel == "b" || cel == "c" ){
-        return "true"
+        return true
     } else {
-        return "false"
+        return false
     }
 }
