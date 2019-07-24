51f8a8749ce8ffcc8c514bb241bff3848bda7016
LuccaVask <52722875+LuccaVask@users.noreply.github.com>
2019-07-24 16:36:40 -0300

    ta indo

---
+++ fase04/ex07/acharNumero.js
@@ -0,0 +1,11 @@
+function acharNumero(vaivai){
+    if (vaivai < 5){
+        return "a"
+    } if (vaivai <= 10){
+        return "b"
+    } if (vaivai <= 100){
+        return "c"
+    } else {
+        return "d"
+    }
+}
