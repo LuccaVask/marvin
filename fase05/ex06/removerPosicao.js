a5434f52f5e16358a077bb365f86d9de27aef797
LuccaVask <52722875+LuccaVask@users.noreply.github.com>
2019-07-30 20:14:13 -0300

    ta foda

--- fase05/ex06/removerPosicao.js
+++ fase05/ex06/removerPosicao.js
@@ -1,8 +1,4 @@
-function removerPosicao(array){
-    var x = array.length
-    var y = x - 1
-    var z = array.toString()
-    var a = z.length
-    var b = a-y
-    return b
+function removerPosicao(array,num){
+    array.splice(num-1,1)
+    return array
 }
