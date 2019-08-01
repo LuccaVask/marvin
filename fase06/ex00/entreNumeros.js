d1411520c80d29cfc9c171821e8a55c2d0550ce0
LuccaVask <52722875+LuccaVask@users.noreply.github.com>
2019-08-01 20:52:33 -0300

    Por enquanto estamos vencendo

---
+++ fase06/ex00/entreNumeros.js
@@ -0,0 +1,13 @@
+function entreNumeros(min,max){
+    if (min<max){
+        while(min<=max){
+        console.log(min)
+        min++
+        }
+    }else{
+        while(max<=min){
+        console.log(max)
+        max++
+        }
+    }
+}
