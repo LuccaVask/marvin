1b8bbf6354fc47a4b7c4f206d56dafecf924db64
LuccaVask <52722875+LuccaVask@users.noreply.github.com>
2019-08-07 16:51:09 -0300

    aos pouquinhos

--- fase06/ex02/por42.js
+++ fase06/ex02/por42.js
@@ -3,23 +3,18 @@ function por42(num1,num2){
         while(num1<=num2){
             if(num1%42 === 0){
                 return num1 + 42
+                num1++
                 break;
-            }else{
-                return false
-                console.log("Não encontrado")
             }
-            num1++
+        num1++
         }
     }else{
         while(num1>=num2){
             if(num2%42 === 0){
                 return num2 + 42
                 break;
-            }else{
-                return false
-                console.log("Não encontrado")
             }
-            num2++
+        num2++
         }
     }
 }
