fd5ee89a3dfec9b69f5af725a6012484453aac53
LuccaVask <52722875+LuccaVask@users.noreply.github.com>
2019-08-07 19:18:05 -0300

    Foi

--- fase06/ex02/por42.js
+++ fase06/ex02/por42.js
@@ -1,7 +1,9 @@
 function por42(num1,num2){
+    var ok = 0
     if(num1<num2){
         while(num1<=num2){
-            if(num1%42 === 0){
+            if(num1%42 === 0 && num1 + 42 <= num2){
+                ok++
                 return num1 + 42
                 num1++
                 break;
@@ -10,11 +12,16 @@ function por42(num1,num2){
         }
     }else{
         while(num1>=num2){
-            if(num2%42 === 0){
+            if(num2%42 === 0 && num2 + 42 <= num1){
+                ok++
                 return num2 + 42
                 break;
             }
         num2++
         }
+    }
+    if(ok === 0){
+        console.log("Número não encontrado")
+        return false
     }
 }
